ef79c1cacdceadeaa2bd96988ebeb98f78b1f9f7
damian-ivanov <clodd@abv.bg>
2021-12-02 12:38:47 +0200

    Fixing a typo in package.json

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,6 +4,16 @@ export default function Login () {
         fetch('http://localhost:3030/jsonstore/collections/books')
         .then(res => res.json())
         .then(data => console.log(data));        
+
+        fetch('http://localhost:3030/users/login', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify('peter@abv.bg', '123456')
+        })
+        .then(res => res.json())
+        .then(data => console.log(data));        
     
     return (
         <>
