18f300f66ea7f8efd672f319cf02930e6c0ee1ee
damian-ivanov <clodd@abv.bg>
2021-12-08 17:26:53 +0200

    Implementing more sorting methods

--- src/components/WishList/WishList.js
+++ src/components/WishList/WishList.js
@@ -14,26 +14,44 @@ const WishList = () => {
             })
     }, []);
 
-    console.log("Begin" + wishes);
-    
-    const orderByLikes = (e) => {
-        e.preventDefault();
-        
-        const sorted = wishes.sort((a, b) => b.likes - a.likes);
- 
-        (async function () {
-            //const sorted = wishes.sort((a, b) => b.likes - a.likes);
-            await setWishes([...sorted]);
-          })();
 
-          console.log("End" + wishes);
-               
+
+    const sort = (e) => {
+        e.preventDefault();
+
+        let sorted;
+
+        switch (e.target.id) {
+            case 'mostLikes':
+                sorted = wishes.sort((a, b) => b.likes - a.likes);
+                break;
+            case 'newest':
+                sorted = wishes.sort((a, b) => new Date(b.date) - new Date(a.date));
+                break;
+            case 'oldest':
+                sorted = wishes.sort((a, b) => new Date(a.date) - new Date(b.date));
+                break;
+            case 'leastLikes':
+                sorted = wishes.sort((a, b) => a.likes - b.likes);
+                break;
+            default:
+                break;
+        }
+
+        (async function () {
+            await setWishes([...sorted]);
+        })();
     };
 
     return (
         <>
             <h3>All submitted wishes:</h3>
-            <div>Order by: <span style={{cursor: "pointer"}} onClick={orderByLikes}>Most likes</span></div>
+            <div>Order by: 
+                <button id="mostLikes" style={{ cursor: "pointer" }} onClick={sort}>Most liked</button>
+                <button id="leastLikes" style={{ cursor: "pointer" }} onClick={sort}>Least liked</button>
+                <button id="newest" style={{ cursor: "pointer" }} onClick={sort}>Newest</button>
+                <button id="oldest" style={{ cursor: "pointer" }} onClick={sort}>Oldest</button>
+            </div>
             {wishes.length > 0
                 ? (
 
