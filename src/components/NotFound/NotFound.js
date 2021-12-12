222b36fc15037c928131a06e95e7bd9cde8f48a6
damian-ivanov <clodd@abv.bg>
2021-12-12 10:23:53 +0200

    Updating the not-found page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,11 +1,13 @@
 import notFound from '../../../src/notFound.png'
 import './NotFound.css';
 
-export default function NotFound () {
-    
+export default function NotFound() {
+
+   window.scrollTo(0, window.innerHeight / 2);
+
     return (
         <div className='notFound'>
-        <img src={notFound} alt="not found"/>
+            <img src={notFound} alt="not found" />
         </div>
     )
 }
