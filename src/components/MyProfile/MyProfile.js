7a1f35840d5bff8429a6bdbf4f105ac09f4731ed
damian-ivanov <clodd@abv.bg>
2021-12-13 23:05:12 +0200

    My profile css adjustments

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -2,13 +2,13 @@ import { useEffect, useState } from "react";
 import { getAuth } from "firebase/auth";
 import * as dataService from '../../services/dataService';
 import WishItem from '../WishItem/WishItem';
+import styles from '../WishList/WishList.module.css';
 
 export default function MyProfile () {
     
     const [wishes, setWishes] = useState([]);
     const auth = getAuth();
     let email = auth.currentUser.email != null ? auth.currentUser.email : "";
-    
 
     useEffect(() => {
         dataService.getAll(email)
@@ -19,19 +19,16 @@ export default function MyProfile () {
 
     return (
         <>
-       
             <h3>My wishes:</h3>
            
             {wishes.length > 0
                 ? (
-
-                    <ul className="wishList">
+                    <ul className={styles.wishList}>
                         {wishes.map(x => <WishItem key={x.id} wish={x} />)}
                     </ul>
                 )
-                : <p className="no-wishes">No wishes in the database!</p>
+                : <h3>You have not submitted any wishes yet!</h3>
             }
-
         </>
     )
 }
