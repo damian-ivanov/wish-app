87bb5ddd2348de981e45291bed55501a06fc9827
damian-ivanov <clodd@abv.bg>
2021-12-14 15:58:09 +0200

    Adjusting header css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,22 @@
 import { useLocation } from 'react-router-dom';
+import styles from './Header.module.css';
+
 export default function Header() {
 
     let location = useLocation();
 
     if (location.pathname !== '/notfound') {
         return (
-            <header className="App-header">
-                <div className="title">
+            <header className={styles.header}>
+                <div>
                     <h1>Share your Christmas wish!</h1>
                     <h3>See the how it is rated by Santa</h3>
                 </div>
-                <img src="https://c.tenor.com/D_gQuXRVQ2wAAAAC/sports-sportsmanias.gif" className="App-logo" alt="logo" />
+                <img src="https://c.tenor.com/D_gQuXRVQ2wAAAAC/sports-sportsmanias.gif" className={styles.logo} alt="logo" />
             </header>
     
         )
     } else {
         return null
     }
-
-   
 }
