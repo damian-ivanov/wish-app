8ae541676caa570c60706e210ee01217564800b0
damian-ivanov <clodd@abv.bg>
2021-12-17 14:34:05 +0200

    New favicon

--- src/components/common/RequireAuth.js
+++ src/components/common/RequireAuth.js
@@ -5,10 +5,10 @@ import { Navigate, Outlet, useLocation } from 'react-router-dom';
 const RequireAuth = () => {
 
     const { user } = useContext(AuthContext);
+
     var location = useLocation();
 
     return user != null ? <Outlet /> : <Navigate to="/login" state={{ prevPath: location.pathname }} />
-
 }
 
 export default RequireAuth;
