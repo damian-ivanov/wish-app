93350dfae2fa281d26b184f85ce00ade03660b7b
damian-ivanov <clodd@abv.bg>
2022-01-25 21:46:16 +0200

    Image attribution

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -8,6 +8,7 @@ export default function ContactMe() {
         <img src={deer} alt="deer"/>
             <p>This site was created as a React JS project for Softuni education by Damian Ivanov.</p>
             <p>You can reach me at damian.i.ivanov@gmail.com, and you can see my other projects at <a href="https://github.com/damian-ivanov" rel="noreferrer" target="_blank">Github</a></p>
+            <a className={styles.attribution} href="https://www.freepik.com/vectors/background">Background vector created by katemangostar - www.freepik.com</a>
             </div>
         </>
     )
