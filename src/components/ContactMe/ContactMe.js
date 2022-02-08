3854a71b047164b3dcfb33da15ec598d8a1a46b2
damian-ivanov <clodd@abv.bg>
2022-02-08 23:39:37 +0200

    CSS adjustments and image optimizations

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,14 +1,13 @@
-import deer from '../../../src/deer.jpg';
+import hat from '../../../src/hat.png';
 import styles from './ContactMe.module.css';
 
 export default function ContactMe() {
     return (
         <>
-        <div className={styles.contact}>
-        <img src={deer} alt="deer"/>
-            <p>This site was created as a React JS project for Softuni education by Damian Ivanov.</p>
-            <p>You can reach me at damian.i.ivanov@gmail.com, and you can see my other projects at <a href="https://github.com/damian-ivanov" rel="noreferrer" target="_blank">Github</a></p>
-            <a className={styles.attribution} href="https://www.freepik.com/vectors/background">Background vector created by katemangostar - www.freepik.com</a>
+            <div className={styles.contact}>
+                <img src={hat} alt="hat" />
+                <p>This site was created as a React JS project as part of my "SoftUni" education.</p>
+                <p>You can reach me at damian.i.ivanov@gmail.com, and you can see my other projects at <a href="https://github.com/damian-ivanov" rel="noreferrer" target="_blank">Github</a></p>
             </div>
         </>
     )
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import { useLocation } from 'react-router-dom';
 import styles from './Header.module.css';
+import santa from '../../../src/santa.gif'
 
 export default function Header() {
 
@@ -12,7 +13,7 @@ export default function Header() {
                     <h1>Share your Christmas wish!</h1>
                     <h3>...and see the how it is rated</h3>
                 </div>
-                <img src="https://c.tenor.com/D_gQuXRVQ2wAAAAC/sports-sportsmanias.gif" className={styles.logo} alt="logo" />
+                <img src={santa} className={styles.logo} alt="logo" />
             </header>
     
         )
