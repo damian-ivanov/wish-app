b9546f0ad3918ef02b0fd14c5014fa382a052cee
damian-ivanov <clodd@abv.bg>
2022-02-09 16:17:31 +0200

    Header typo fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,7 @@ export default function Header() {
             <header className={styles.header}>
                 <div>
                     <h1>Share your Christmas wish!</h1>
-                    <h3>...and see the how it is rated</h3>
+                    <h3>...and see how it is rated</h3>
                 </div>
                 <img src={santa} className={styles.logo} alt="logo" />
                 <p>Gif source: <a href="https://tenor.com/view/sports-sportsmanias-emoji-animated-emojis-merry-christmas-gif-19449254" target="_blank" rel="noreferrer">Tenor</a></p>
