d555d9fc07e032d26c47e027433c627515b4e6aa
damian-ivanov <clodd@abv.bg>
2022-06-08 15:26:45 +0300

    Adding a random joke API

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,7 +1,22 @@
 import hat from '../../../src/hat.png';
 import styles from './ContactMe.module.css';
+import { useEffect, useState } from "react";
+import loader from '../../../src/loader.gif';
 
 export default function ContactMe() {
+
+const [joke, setJoke] = useState(null);
+const [isReady, setIsReady] = useState(false);
+
+useEffect(() => {
+    fetch('https://v2.jokeapi.dev/joke/any?safe-mode')
+    .then((response) => response.json())
+    .then((data) => setJoke(data))
+    .then(() => setIsReady(true))
+    .catch((error) => console.error(error))
+}, [isReady] );
+
+
     return (
         <>
             <div className={styles.contact}>
@@ -9,6 +24,11 @@ export default function ContactMe() {
                 <p>This site was created as a React JS project as part of my "SoftUni" education.</p>
                 <p>You can reach me at damian.i.ivanov@gmail.com, and you can see my other projects at <a href="https://github.com/damian-ivanov" rel="noreferrer" target="_blank">Github</a></p>
             </div>
+            {joke != null ? <article><p style={{color:"darkseagreen"}}> Here is a random joke:</p>
+                <p>{joke.setup}</p>
+                <b>{joke.delivery}</b>
+            </article>
+            : <img src={loader} alt='loading'></img>}
         </>
     )
 }
